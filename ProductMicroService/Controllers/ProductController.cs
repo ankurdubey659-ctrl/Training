2e1c2107b6f70dc20f00486a9e9ed08eef23dba9
Dubey <ankur.b.dubey@capgemini.com>
2024-12-23 23:34:32 +0530

    Initial commit: Added ProductMicroService

---
+++ ProductMicroService/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductMicroService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private static List<Product> products = new List<Product>
+    {
+        new Product { Id = 1, Name = "Laptop", Price = 1000 },
+        new Product { Id = 2, Name = "Phone", Price = 500 }
+    };
+
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts() => Ok(products);
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+}
